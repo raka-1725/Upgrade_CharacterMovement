1922a99437bdd3f7bb603f096acf85463cae7959
Stanfield <bstanfie@student.uiwtx.edu>
2025-10-14 16:13:55 -0500

    added player movement

--- Assets/_MyAssets/Player/Scripts/SPlayerMovement.cs
+++ Assets/_MyAssets/Player/Scripts/SPlayerMovement.cs
@@ -7,16 +7,27 @@ public class SPlayerMovement : MonoBehaviour
     private PlayerInputSystem mPlayerInputSystem;
     private CharacterController mCharacterController;
 
-    [Header("Jump Settings")]
+    [Header("Player Jump Settings")]
     [SerializeField] private float mPlayerJumpForce = 5f;
     [SerializeField] private float mPlayerFallSpeed = 50f;
     private Vector3 mVerticalVelocity;
-    
+
+    [Header("Player Movement Settings")]
+    [SerializeField] private float mPlayerMoveSpeed = 5;
+    private Vector3 mHorizontalVelocity;
+    private Vector2 mMoveInput;
     private void Awake()
     {
         mPlayerInputSystem = new PlayerInputSystem();
         mPlayerInputSystem.Gameplay.Jump.performed += PerformedJump;
+        mPlayerInputSystem.Gameplay.Move.performed += PlayerMovement;
+        mPlayerInputSystem.Gameplay.Move.canceled += PlayerMovement;
         mCharacterController = GetComponent<CharacterController>();
+    }
+    private void PlayerMovement(InputAction.CallbackContext context)
+    {
+        mMoveInput = context.ReadValue<Vector2>();
+        Debug.Log($"Player is Moving");
     }
     private void OnEnable()
     {
@@ -40,6 +51,11 @@ public class SPlayerMovement : MonoBehaviour
         {
             mVerticalVelocity.y += Physics.gravity.y * Time.deltaTime;
         }
-        mCharacterController.Move(mVerticalVelocity * Time.deltaTime);
+        mHorizontalVelocity += mMoveInput.x * Camera.main.transform.right * mPlayerMoveSpeed * Time.deltaTime;
+        mHorizontalVelocity += mMoveInput.y * Camera.main.transform.forward * mPlayerMoveSpeed * Time.deltaTime;
+        mHorizontalVelocity = Vector3.ClampMagnitude(mHorizontalVelocity, mPlayerMoveSpeed);
+
+        Vector3 FinalMov = mHorizontalVelocity + mVerticalVelocity;
+        mCharacterController.Move(FinalMov * Time.deltaTime);
     }
 }
