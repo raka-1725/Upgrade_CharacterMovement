f8addc970b63541df8ef0fb9f477272655514b90
Ryoya Akaike <akaike@student.uiwtx.edu>
2025-10-29 17:47:54 -0500

    UI visualization for interaction

--- Assets/_MyAssets/Interactivity/SInteractability.cs
+++ Assets/_MyAssets/Interactivity/SInteractability.cs
@@ -1,15 +1,25 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class SInteractability : MonoBehaviour
 {
     [Header("References")]
     private PlayerInputSystem mPlayerInputSystem;
+    private SUIController mUI;
 
     private void Awake()
     {
         mPlayerInputSystem = new PlayerInputSystem();
 
         mPlayerInputSystem.Gameplay.Interaction.performed += PlayerInteraction;
+        mPlayerInputSystem.Gameplay.Interaction.canceled += PlayerInteractionIdle;
+
+        mUI = FindAnyObjectByType<SUIController>();
+    }
+
+    private void PlayerInteractionIdle(InputAction.CallbackContext context)
+    {
+        mUI.UpdateInteractablityUI(false);
     }
     #region Enable/disable
     private void OnEnable()
@@ -25,5 +35,8 @@ public class SInteractability : MonoBehaviour
     {
         //use the text in the line below in the parenthesis to print anything you write out when you press the F key!!
         Debug.Log($"Put Anything here to print it!!");
+
+        mUI.UpdateInteractablityUI(true);
     }
+
 }
